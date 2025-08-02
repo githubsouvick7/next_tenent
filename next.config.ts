78eb35fec3be9ed92952303388d4e9c9bc3d357f
Souvick Chakraborty <hellosouvickk@gmail.com>
2025-08-02 17:58:04 +0530

    start

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverActions: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-07d8598045444efc9676b80f08ab88fe.r2.dev",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "image.jobspring.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/images/**",
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
