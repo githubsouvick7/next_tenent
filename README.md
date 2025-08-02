d85337684134660f2cef6927de1766979a7c2984
Souvick Chakraborty <hellosouvickk@gmail.com>
2025-08-02 18:29:34 +0530

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-Fimon.app = Financial Intelligence Monitor
--- src/app/template.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const template = ({children}: { children: React.ReactNode }) => {
-  return (
-    <div>{children}</div>
-  )
-}
-
-export default template
